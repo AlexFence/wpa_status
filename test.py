75712d1979c41c123b2f29f7cf42d8f41d08af57
Alex Fence <alexfence.code@gmail.com>
2019-12-26 17:01:35 +0100

    make flake8 happy

--- test.py
+++ test.py
@@ -2,29 +2,8 @@ import wpa_status
 
 client = wpa_status.Client("/tmp/wifi-chan.sok")
 
+print(wpa_status.ping(client).serialize())
 
-def ping(client):
-    response = client.request(wpa_status.Method.PING)
-    return response
+print(wpa_status.list_networks(client).serialize())
 
-
-def status(client):
-    response = client.request(wpa_status.Method.STATUS)
-    return response
-
-
-def list_n(client):
-    response = client.request(wpa_status.Method.LIST_NETWORKS)
-    return response
-
-
-print(ping(client).serialize())
-
-
-client = wpa_status.Client("/tmp/wifi-chan.sok")
-
-print(list_n(client).serialize())
-
-
-client = wpa_status.Client("/tmp/wifi-chan.sok")
-print(status(client).serialize())
+print(wpa_status.status(client).serialize())
