c3cb14e72591e3a4f801e57cbc36ac0a90b3d36c
Alex Fence <alexfence.code@gmail.com>
2019-12-26 17:01:02 +0100

    add some functions

--- wpa_status/__init__.py
+++ wpa_status/__init__.py
@@ -1,2 +1,18 @@
 from wpa_status.client import Client
 from wpa_status.protocol import Method
+
+
+def ping(client: Client):
+    return client.request(Method.PING)
+
+
+def status(client: Client):
+    return client.request(Method.STATUS)
+
+
+def list_networks(client: Client):
+    return client.request(Method.LIST_NETWORKS)
+
+
+def is_supplicant_running(client: Client):
+    return client.request(Method.SUPPLICANT_RUNNING)
