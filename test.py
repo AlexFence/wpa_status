4dd27f7825a710925245a4550c33b2e313283058
Alex Fence <alexfence.code@gmail.com>
2019-12-31 13:59:44 +0100

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import wpa_status
-
-client = wpa_status.Client("/tmp/wifi-chan.sok")
-
-print(wpa_status.ping(client).serialize())
-
-print(wpa_status.list_networks(client).serialize())
-
-print(wpa_status.status(client).serialize())
